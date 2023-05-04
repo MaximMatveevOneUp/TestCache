1cf99112894f1fa3a2a610f94d4043671ed319d5
Maxim Matveev <maxim.m@oneupgames.gg>
2023-05-04 14:58:20 +0300

    Inital commit

---
+++ Assets/Editor/Builder.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Code.GGClient.Build.Editor
+{
+    public static class Builder
+    {
+        // ReSharper disable once UnusedMember.Global
+        public static void ApplySettings()
+        {
+            Debug.Log("ApplySettings invoked");
+        }
+
+        // ReSharper disable once UnusedMember.Global
+        public static void Build()
+        {
+            Debug.Log("Build invoked");
+        }
+    }
+}
